3a90a3b664238f0bebf2b37768916363f128eaae
HAAS <marcelo@hpbi.com.br>
2026-02-11 14:52:19 -0300

    Update .env.js
    Ajuste para Chatwoot + Kanban

--- public/.env.js
+++ public/.env.js
@@ -1,8 +1,9 @@
 //.env.js
 // Este arquivo substitui o uso do .env para variáveis de ambiente no frontend
+// ALTERADO em 2026-02-11
 window._env_ = {
-  REACT_APP_CHATWOOT_TOKEN: 'xxxxxxzxxxxxxxxxxxxxx',
-  REACT_APP_CHATWOOT_ACCOUNT_ID: '1',
-  REACT_APP_CHATWOOT_URL: 'https://xxxxxxxxxxx.com',
-  REACT_APP_DEBUG: 'true',
+  REACT_APP_CHATWOOT_TOKEN: '',
+  REACT_APP_CHATWOOT_ACCOUNT_ID: '',
+  REACT_APP_CHATWOOT_URL: '',
+  REACT_APP_DEBUG: 'false',
 };
